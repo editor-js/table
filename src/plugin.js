8c19f8800e591964950731b206b34c862c56a147
valerii_metlenok <metljonok@adn.agency>
2019-08-15 15:22:33 +0700

    fix table cell

--- src/plugin.js
+++ src/plugin.js
@@ -74,7 +74,7 @@ class Table {
       if (isWorthless) {
         continue;
       }
-      data.push(cols.map(cell => cell.innerHTML));
+      data.push(cols.map(cell => cell.querySelector(`.${CSS.input}`).innerHTML));
     }
 
     return {
