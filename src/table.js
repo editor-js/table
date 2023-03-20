b2fe286728590dc6694718323fd4cb60ec87763b
MotchQF <127864796+MotchQF@users.noreply.github.com>
2023-03-20 14:02:59 -0300

    Updated withHeadings to respect config definition

--- src/table.js
+++ src/table.js
@@ -74,7 +74,7 @@ export default class Table {
 
     // Additional settings for the table
     this.tunes = {
-      withHeadings: false
+      withHeadings: config?.withHeadings ? true : false
     };
 
     /**
