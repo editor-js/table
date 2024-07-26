f78ca95396fbe24ba3089ffb31f8a8d2aec5dcc4
Fabian <edv@sozarch.uzh.ch>
2024-07-26 09:32:02 +0200

    jsdoc types added

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import path from "path";
 import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 import * as pkg from "./package.json";
+import dts from 'vite-plugin-dts';
 
 const NODE_ENV = process.argv.mode || "development";
 const VERSION = pkg.version;
@@ -19,5 +20,8 @@ export default {
     VERSION: JSON.stringify(VERSION),
   },
 
-  plugins: [cssInjectedByJsPlugin({useStrictCSP: true})],
+  plugins: [
+    cssInjectedByJsPlugin({useStrictCSP: true}),
+    dts({tsconfigPath: './tsconfig.json'})
+  ]
 };
